29495aa1ece56a8f8be1297ecada1bbf947d2e71
yukinorei <>
2018-12-04 07:17:10 +0900

    console.logにスコアが表示されるようコード追加

--- public/js/components/TypingTutorGame.js
+++ public/js/components/TypingTutorGame.js
@@ -19,13 +19,31 @@ class TypingTutorGame {
     this.isRoundInProgress = true;
     this.currentStrokeCount = -1;
     this.initTargetText();
+    this.correctChar = 0;
+    this.wrongChar = 0;
+    this.targetText = null;
+  }
+
+  displayScore(correctChar, targetTotalCountChar) {
+    const accuracyRate = Math.round(correctChar / targetTotalCountChar * 100);
+    console.log(`${accuracyRate}%正解`);
   }
 
   handleKeyStroke(key) {
     if (!this.isRoundInProgress) return;
     this.currentStrokeCount += 1;
     const targetChar = this.targetText[this.currentStrokeCount];
+    if (key === targetChar) {
+      this.correctChar += 1;
+    } else {
+      this.wrongChar += 1;
+    }
     this.view.renderKeystroke(key, targetChar);
+    const targetTotalCountChar = this.targetText.length;
+    if (targetTotalCountChar === this.correctChar + this.wrongChar) {
+      this.isRoundInProgress = false;
+      this.displayScore(this.correctChar, targetTotalCountChar);
+    }
   }
 
   initTargetText() {
